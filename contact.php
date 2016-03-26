f5e9f8547be445c0369e451c282e533439c69ff2
Tomer Achdut <Tomer Achdut>
2016-03-26 18:52:11 +0300

    php changed

--- contact.php
+++ contact.php
@@ -72,7 +72,7 @@ if(isset($_POST['email'])) {
 	 
 	'X-Mailer: PHP/' . phpversion();
 	 
-	@mail($email_to, $email_subject, $email_message, $headers);  
+	mail($email_to, $email_subject, $email_message, $headers);  
 
 
 ?>
